1f136c33246899153421b094958c1115d287a0ad
JoelKanyi <joelkanyi98@gmail.com>
2021-01-28 21:02:14 +0300

    functionality

--- app/src/main/java/com/kanyideveloper/navigationdemo/MainActivity.java
+++ app/src/main/java/com/kanyideveloper/navigationdemo/MainActivity.java
@@ -1,14 +1,47 @@
 package com.kanyideveloper.navigationdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView mBottomNavigationView = findViewById(R.id.bottomNavigationView);
+        DrawerLayout mDrawer = findViewById(R.id.drawerLayout);
+        NavigationView mNavigationView = findViewById(R.id.navigationView);
+
+        navController = Navigation.findNavController(this,R.id.fragment);
+
+        //setup bottom navigation
+        NavigationUI.setupWithNavController(mBottomNavigationView,navController);
+
+        //Setup Top Back post_style_button
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).setOpenableLayout(mDrawer).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, mDrawer);
+
+        //Setup Navigation Drawer
+        NavigationUI.setupWithNavController(mNavigationView, navController);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController,appBarConfiguration);
     }
 }
